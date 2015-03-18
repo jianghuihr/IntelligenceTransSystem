67c225ef6590b644e8dd70cdd1c76e1e4a248a65
jianghuihr <jianghhuarui@gmailcom>
2015-03-18 16:57:22 +0800

    粗略框架设计

---
+++ src/com/just/jh/intelligencetranssystem/activity/RouteFragment.java
@@ -0,0 +1,20 @@
+package com.just.jh.intelligencetranssystem.activity;
+
+import com.just.jh.intelligencetranssystem.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RouteFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_route, container, false);
+	}
+	
+
+}
